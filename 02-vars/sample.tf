a8cae428c98fbb35b1ee50a05a54a31f14905004
Rakesh <dasarirakeshnaidu@gmail.com>
2023-05-18 18:12:38 +0530

    auto git push

--- 02-vars/sample.tf
+++ 02-vars/sample.tf
@@ -11,3 +11,29 @@ output "sample_output" {
 }
 
 #  A variable can be accessed without ${} as well, if it's not accompanied in any sentence. If you just want to use it, you can directly refer it as `var.VariableName`
+
+variable "integer" {
+    default = 1000
+}
+
+output "number" {
+    value = var.integer 
+}
+
+# List variable 
+variable "sample_list" {
+    default = [
+        "Terraform",
+        "Training",
+        "Pulumi",
+        "Cloud Formation",
+        120,
+        true,
+        "ManuVerma"
+    ]
+}
+
+# From the above list block, data in that block can be of any type 
+output "sample_list_op" {
+    value = "Welcome to ${var.sample_list[0]} Training and duration of the training is  ${var.sample_list[4]} hours"
+}
